029cf068da80b202f921707498d2ee8ba95f16b1
CloudKlass <104594551+CloudKlass@users.noreply.github.com>
2022-08-18 11:24:11 +0100

    labrename

---
+++ solutions/lab2/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.18.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "lab2" {
+  name     = "RG-Lab2"
+  location = "West Europe"
+}
+
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.1.0.0/16"]
+  subnet_prefixes     = ["10.1.1.0/24"]
+  subnet_names        = ["lab2-subnet"]
+
+  subnet_service_endpoints = {
+    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
+    subnet3 = ["Microsoft.AzureActiveDirectory"]
+  }
+
+  tags = {
+    environment = "lab2"
+    }
+
+  depends_on = [azurerm_resource_group.lab2]
+}
+
+# Do not edit above this line - The above code has been provided so that you may concentrate on building a VM and not be concerned with Provider, Resource Group or VNET at this time
+
---
+++ solutions/lab6/variables.tf
@@ -0,0 +1,8 @@
+variable "location" {
+    default = "westeurope"
+    type = string
+}
+variable "client_id" {}
+
+variable "client_secret"{}
+    
